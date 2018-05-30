4586c671d1fb3ecaa5acc6b508be6999bf578d59
Ines <35921204+InesAtia@users.noreply.github.com>
2018-05-30 20:57:08 +0200

    izmjena
    izmjena; admin panel; osoblje panel

---
+++ admin.php
@@ -0,0 +1,54 @@
+<?php include 'header_admin.php'; ?> 
+
+	<!--<div class="container page">
+		
+   <h3>Proizvodi</h3>
+		<hr>
+
+	<form>
+		<div class="row">
+				<div class="col-12">
+							<label>Naziv</label>
+							<input type="text" name="Naziv" class="form-control">	
+						</div>
+		
+
+
+		<div class="col-12">
+			<label>Opis</label>
+			<input type="text" name="opis" class="form-control">	
+						</div>
+
+		<div class="col-6">
+			<label>Cijena</label>
+			<input type="text" name="cijena" class="form-control">	
+						</div>
+
+		<div class="col-6">
+
+				<label>Tip</label>
+				<select class="form-control" name="tip">
+			    <option value="kineska">Kineska</option>
+			    <option value="meksicka">Meksička</option>
+			    <option value="talijanska">Italijanska</option>
+			    <option value="bosanska">Bosanska</option>
+			  </select>
+			</div>
+
+		<div class="col-12"> <hr> </div>
+		<div class="col-12">
+
+			<button type="submit" class="btn btn-danger">Snimi</button>
+		</div>
+
+		</div>
+	</form>	
+	<br>
+</div> -->
+
+
+
+
+
+<?php include 'footer_admin.php'; ?> 
+
---
+++ osoblje.php
@@ -0,0 +1,10 @@
+<?php include 'header_osoblje.php'; ?> 
+
+
+
+
+
+
+
+<?php include 'footer_osoblje.php'; ?> 
+
